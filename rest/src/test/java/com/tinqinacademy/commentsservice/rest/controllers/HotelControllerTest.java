d6dc8f8f6c4a72fbd848fe66cbcb8cb985bb6439
yordanov0502 <yordanovtodor281@gmail.com>
2024-08-18 18:55:08 +0300

    [T11] addCommentForRoom method added to CommentsRestExport

--- rest/src/test/java/com/tinqinacademy/commentsservice/rest/controllers/HotelControllerTest.java
+++ rest/src/test/java/com/tinqinacademy/commentsservice/rest/controllers/HotelControllerTest.java
@@ -22,162 +22,162 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @AutoConfigureMockMvc
 class HotelControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-    void getAllCommentsOfRoomOk() throws Exception {
-        String roomId = "11A";
-
-        MvcResult getAllRoomComments = mvc.perform(get(RestApiRoutes.GET_ALL_ROOM_COMMENTS,roomId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding("UTF-8"))
-                .andReturn();
-
-        assertEquals(200,getAllRoomComments.getResponse().getStatus());
-    }
-
-    @Test
-    void getAllCommentsOfRoomNotFound() throws Exception {
-        String roomId = "11A";
-
-        MvcResult getAllRoomComments = mvc.perform(get(RestApiRoutes.GET_ALL_ROOM_COMMENTS+"/wrong",roomId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding("UTF-8"))
-                .andReturn();
-
-        assertEquals(404,getAllRoomComments.getResponse().getStatus());
-    }
-
-    @Test
-    void addCommentForRoomCreated() throws Exception {
-
-        String roomId = "11A";
-
-        AddCommentForRoomInput input = AddCommentForRoomInput.builder()
-                .roomId(roomId)
-                .firstName("Todor")
-                .lastName("Yordanov")
-                .content("The room was cozy and shiny.")
-                .build();
-
-        String serializedInput = objectMapper.writeValueAsString(input);
-
-        MvcResult addCommentForRoom = mvc.perform(post(RestApiRoutes.ADD_COMMENT_FOR_ROOM,roomId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(serializedInput)
-                        .characterEncoding("UTF-8"))
-                .andReturn();
-
-        assertEquals(201,addCommentForRoom.getResponse().getStatus());
-    }
-
-    @Test
-    void addCommentForRoomBadRequest() throws Exception {
-
-        String roomId = "11A";
-
-        AddCommentForRoomInput input = AddCommentForRoomInput.builder()
-                .roomId(roomId)
-                .firstName("")
-                .lastName("Yordanov")
-                .content("The room was cozy and shiny.")
-                .build();
-
-        String serializedInput = objectMapper.writeValueAsString(input);
-
-        MvcResult addCommentForRoom = mvc.perform(post(RestApiRoutes.ADD_COMMENT_FOR_ROOM,roomId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(serializedInput)
-                        .characterEncoding("UTF-8"))
-                .andReturn();
-
-        assertEquals(400,addCommentForRoom.getResponse().getStatus());
-    }
-
-    @Test
-    void addCommentForRoomNotFound() throws Exception {
-
-        String roomId = "11A";
-
-        AddCommentForRoomInput input = AddCommentForRoomInput.builder()
-                .roomId(roomId)
-                .firstName("Todor")
-                .lastName("Yordanov")
-                .content("The room was cozy and shiny.")
-                .build();
-
-        String serializedInput = objectMapper.writeValueAsString(input);
-
-        MvcResult addCommentForRoom = mvc.perform(post(RestApiRoutes.ADD_COMMENT_FOR_ROOM+"/wrong",roomId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(serializedInput)
-                        .characterEncoding("UTF-8"))
-                .andReturn();
-
-        assertEquals(404,addCommentForRoom.getResponse().getStatus());
-    }
-
-
-    @Test
-    void editCommentForRoomOk() throws Exception {
-        String commentId = "1234";
-
-        UserEditCommentForRoomInput input = UserEditCommentForRoomInput.builder()
-                .commentId(commentId)
-                .content("The room was cozy, shiny and large.")
-                .build();
-
-        String serializedInput = objectMapper.writeValueAsString(input);
-
-        MvcResult editCommentForRoom = mvc.perform(put(RestApiRoutes.EDIT_COMMENT_FOR_ROOM,commentId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(serializedInput)
-                        .characterEncoding("UTF-8"))
-                .andReturn();
-
-        assertEquals(200,editCommentForRoom.getResponse().getStatus());
-    }
-
-    @Test
-    void editCommentForRoomBadRequest() throws Exception {
-        String commentId = "1234";
-
-        UserEditCommentForRoomInput input = UserEditCommentForRoomInput.builder()
-                .commentId(commentId)
-                .content("")
-                .build();
-
-        String serializedInput = objectMapper.writeValueAsString(input);
-
-        MvcResult editCommentForRoom = mvc.perform(put(RestApiRoutes.EDIT_COMMENT_FOR_ROOM,commentId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(serializedInput)
-                        .characterEncoding("UTF-8"))
-                .andReturn();
-
-        assertEquals(400,editCommentForRoom.getResponse().getStatus());
-    }
-
-    @Test
-    void editCommentForRoomNotFound() throws Exception {
-        String commentId = "1234";
-
-        UserEditCommentForRoomInput input = UserEditCommentForRoomInput.builder()
-                .commentId(commentId)
-                .content("The room was cozy, shiny and large.")
-                .build();
-
-        String serializedInput = objectMapper.writeValueAsString(input);
-
-        MvcResult editCommentForRoom = mvc.perform(put(RestApiRoutes.EDIT_COMMENT_FOR_ROOM+"/wrong",commentId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(serializedInput)
-                        .characterEncoding("UTF-8"))
-                .andReturn();
-
-        assertEquals(404,editCommentForRoom.getResponse().getStatus());
-    }
+//    @Autowired
+//    private MockMvc mvc;
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Test
+//    void getAllCommentsOfRoomOk() throws Exception {
+//        String roomId = "11A";
+//
+//        MvcResult getAllRoomComments = mvc.perform(get(RestApiRoutes.GET_ALL_ROOM_COMMENTS,roomId)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .characterEncoding("UTF-8"))
+//                .andReturn();
+//
+//        assertEquals(200,getAllRoomComments.getResponse().getStatus());
+//    }
+//
+//    @Test
+//    void getAllCommentsOfRoomNotFound() throws Exception {
+//        String roomId = "11A";
+//
+//        MvcResult getAllRoomComments = mvc.perform(get(RestApiRoutes.GET_ALL_ROOM_COMMENTS+"/wrong",roomId)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .characterEncoding("UTF-8"))
+//                .andReturn();
+//
+//        assertEquals(404,getAllRoomComments.getResponse().getStatus());
+//    }
+//
+//    @Test
+//    void addCommentForRoomCreated() throws Exception {
+//
+//        String roomId = "11A";
+//
+//        AddCommentForRoomInput input = AddCommentForRoomInput.builder()
+//                .roomId(roomId)
+//                .firstName("Todor")
+//                .lastName("Yordanov")
+//                .content("The room was cozy and shiny.")
+//                .build();
+//
+//        String serializedInput = objectMapper.writeValueAsString(input);
+//
+//        MvcResult addCommentForRoom = mvc.perform(post(RestApiRoutes.ADD_COMMENT_FOR_ROOM,roomId)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(serializedInput)
+//                        .characterEncoding("UTF-8"))
+//                .andReturn();
+//
+//        assertEquals(201,addCommentForRoom.getResponse().getStatus());
+//    }
+//
+//    @Test
+//    void addCommentForRoomBadRequest() throws Exception {
+//
+//        String roomId = "11A";
+//
+//        AddCommentForRoomInput input = AddCommentForRoomInput.builder()
+//                .roomId(roomId)
+//                .firstName("")
+//                .lastName("Yordanov")
+//                .content("The room was cozy and shiny.")
+//                .build();
+//
+//        String serializedInput = objectMapper.writeValueAsString(input);
+//
+//        MvcResult addCommentForRoom = mvc.perform(post(RestApiRoutes.ADD_COMMENT_FOR_ROOM,roomId)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(serializedInput)
+//                        .characterEncoding("UTF-8"))
+//                .andReturn();
+//
+//        assertEquals(400,addCommentForRoom.getResponse().getStatus());
+//    }
+//
+//    @Test
+//    void addCommentForRoomNotFound() throws Exception {
+//
+//        String roomId = "11A";
+//
+//        AddCommentForRoomInput input = AddCommentForRoomInput.builder()
+//                .roomId(roomId)
+//                .firstName("Todor")
+//                .lastName("Yordanov")
+//                .content("The room was cozy and shiny.")
+//                .build();
+//
+//        String serializedInput = objectMapper.writeValueAsString(input);
+//
+//        MvcResult addCommentForRoom = mvc.perform(post(RestApiRoutes.ADD_COMMENT_FOR_ROOM+"/wrong",roomId)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(serializedInput)
+//                        .characterEncoding("UTF-8"))
+//                .andReturn();
+//
+//        assertEquals(404,addCommentForRoom.getResponse().getStatus());
+//    }
+//
+//
+//    @Test
+//    void editCommentForRoomOk() throws Exception {
+//        String commentId = "1234";
+//
+//        UserEditCommentForRoomInput input = UserEditCommentForRoomInput.builder()
+//                .commentId(commentId)
+//                .content("The room was cozy, shiny and large.")
+//                .build();
+//
+//        String serializedInput = objectMapper.writeValueAsString(input);
+//
+//        MvcResult editCommentForRoom = mvc.perform(put(RestApiRoutes.EDIT_COMMENT_FOR_ROOM,commentId)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(serializedInput)
+//                        .characterEncoding("UTF-8"))
+//                .andReturn();
+//
+//        assertEquals(200,editCommentForRoom.getResponse().getStatus());
+//    }
+//
+//    @Test
+//    void editCommentForRoomBadRequest() throws Exception {
+//        String commentId = "1234";
+//
+//        UserEditCommentForRoomInput input = UserEditCommentForRoomInput.builder()
+//                .commentId(commentId)
+//                .content("")
+//                .build();
+//
+//        String serializedInput = objectMapper.writeValueAsString(input);
+//
+//        MvcResult editCommentForRoom = mvc.perform(put(RestApiRoutes.EDIT_COMMENT_FOR_ROOM,commentId)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(serializedInput)
+//                        .characterEncoding("UTF-8"))
+//                .andReturn();
+//
+//        assertEquals(400,editCommentForRoom.getResponse().getStatus());
+//    }
+//
+//    @Test
+//    void editCommentForRoomNotFound() throws Exception {
+//        String commentId = "1234";
+//
+//        UserEditCommentForRoomInput input = UserEditCommentForRoomInput.builder()
+//                .commentId(commentId)
+//                .content("The room was cozy, shiny and large.")
+//                .build();
+//
+//        String serializedInput = objectMapper.writeValueAsString(input);
+//
+//        MvcResult editCommentForRoom = mvc.perform(put(RestApiRoutes.EDIT_COMMENT_FOR_ROOM+"/wrong",commentId)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(serializedInput)
+//                        .characterEncoding("UTF-8"))
+//                .andReturn();
+//
+//        assertEquals(404,editCommentForRoom.getResponse().getStatus());
+//    }
 }
--- restexport/src/main/java/com/tinqinacademy/commentsservice/restexport/CommentsRestExport.java
+++ restexport/src/main/java/com/tinqinacademy/commentsservice/restexport/CommentsRestExport.java
@@ -1,14 +1,20 @@
 package com.tinqinacademy.commentsservice.restexport;
 
 import com.tinqinacademy.commentsservice.api.RestApiRoutes;
+import com.tinqinacademy.commentsservice.api.operations.hotel.addcommentforroom.AddCommentForRoomInput;
+import com.tinqinacademy.commentsservice.api.operations.hotel.addcommentforroom.AddCommentForRoomOutput;
 import com.tinqinacademy.commentsservice.api.operations.hotel.getallcommentsofroom.GetAllCommentsOfRoomOutput;
 import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Headers({"Content-Type: application/json"})
 public interface CommentsRestExport {
 
     @RequestLine("GET "+ RestApiRoutes.GET_ALL_ROOM_COMMENTS)
     GetAllCommentsOfRoomOutput getAllCommentsForRoom(@Param String roomId);
+
+    @RequestLine("POST /api/v1/hotel/{roomId}/comment")
+    AddCommentForRoomOutput addCommentForRoom(@Param String roomId, @RequestBody AddCommentForRoomInput input);
 }
